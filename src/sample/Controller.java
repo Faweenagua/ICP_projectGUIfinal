a275a08b50ae7b1312486265764ae74e9b260507
Faweenagua <46810972+Faweenagua@users.noreply.github.com>
2019-03-20 22:15:07 +0000

    new commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,16 +9,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -26,7 +21,7 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
 
-    public TextField obseName;
+    public TextField obseName = new TextField();
     public TextField observeCountry;
     public TextField observeYear;
     public TextField observeArea;
@@ -36,33 +31,66 @@ public class Controller implements Initializable {
     public TextField eventYear;
     public TextField siteLatitude;
 
+    public ChoiceBox<String> selectObservatory = new ChoiceBox<>();
+    public ChoiceBox<String> vegetationColour = new ChoiceBox<>();
+    public Label biggestColorValue = new Label();
+    public Label maxGalamObservatory = new Label();
+    public ChoiceBox<String> somethingThan = new ChoiceBox<String>();
+    public ChoiceBox<String> colourValue = new ChoiceBox<String>();
+
     public void addObservatory(Event event) throws IOException {
         String name = obseName.textProperty().get();
         String country = observeCountry.textProperty().get();
         String year = observeYear.textProperty().get();
         String area = observeArea.textProperty().get();
+
+        Observatory observatory = new Observatory();
+        observatory.setName(name);
+        observatory.setCountry(country);
+        observatory.setYearStarted(Integer.parseInt(year));
+        observatory.setAreaInSquareMeters(Double.parseDouble(area));
+
+        Monitoring.observatories.add(observatory);
+
         backHome(event);
+    }
+
+    public void showGalamseyEvents(Event event){
+        String relationOption = somethingThan.getSelectionModel().getSelectedItem();
+        String colourType = colourValue.getSelectionModel().getSelectedItem();
+
+
     }
 
     public void addGalamseyEvent(Event event) throws IOException {
+        String observatoryName = selectObservatory.getSelectionModel().getSelectedItem();
+        String colourOfVegetation = vegetationColour.getSelectionModel().getSelectedItem();
         String latitude = siteLatitude.textProperty().get();
         String longitude = siteLatitude.textProperty().get();
-        String yearEvent = eventYear.textProperty().get();
-        String area = eventYear.textProperty().get();
+        String yearOfEvent = eventYear.textProperty().get();
+
+        Galamsey galamsey = new Galamsey();
+        galamsey.setVegetationLatitude(Double.parseDouble(latitude));
+        galamsey.setVegetationLongitude(Double.parseDouble(longitude));
+        galamsey.setVegetationColour(colourOfVegetation);
+        galamsey.setEventYear(Integer.parseInt(yearOfEvent));
+
+
+
+        Monitoring.observatories.get(getIndexOfObservatory(observatoryName)).addEvent(galamsey);
+
+        System.out.println();
+
         backHome(event);
-        System.out.println(latitude+longitude+area);
     }
 
 
-    @FXML
-    private TableView<Observatory> observeList;
+    @FXML private TableView<Observatory> observeList = new TableView<>();
 
 
-    @FXML
-    private TableColumn<Observatory, String> observeName;
+    @FXML private TableColumn<Observatory, String> observeName = new TableColumn<>();
 
-    @FXML
-    private TableColumn<Observatory, Integer> numberOfEvents;
+    @FXML private TableColumn<Observatory, Integer> numberOfEvents = new TableColumn<>();
 
 
 
@@ -87,6 +115,8 @@ public class Controller implements Initializable {
         Scene recordsScene = new Scene(records);
         Stage recordsStage = (Stage)((Node) event.getSource()).getScene().getWindow();
         recordsStage.setScene(recordsScene);
+
+
         recordsStage.show();
     }
 
@@ -100,15 +130,53 @@ public class Controller implements Initializable {
     }
 
 
+    private ObservableList<String> observa;
+
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-      //observeName.setCellValueFactory(new PropertyValueFactory<Observatory, String>("name"));
-      //numberOfEvents.setCellValueFactory(new PropertyValueFactory<Observatory, Integer>("numberOfGalamseyEvents"));
+        observa = FXCollections.observableArrayList();
 
-      //observeList.setItems(getObservatories());
+        obsList();
+        selectObservatory.setItems(observa);
+        selectObservatory.setValue("Select an Observatory");
 
+        vegetationColour.getItems().addAll("Select a colour","Yellow", "Green", "Brown");
+        vegetationColour.setValue("Select a colour");
+
+        somethingThan.getItems().addAll("Select option","Greater than","Equal to", "Less than");
+        somethingThan.setValue("Select option");
+
+        colourValue.getItems().addAll("Select a colour","Yellow", "Green", "Brown");
+        colourValue.setValue("Select a colour");
+
+
+      observeName.setCellValueFactory(new PropertyValueFactory<Observatory, String>("name"));
+      numberOfEvents.setCellValueFactory(new PropertyValueFactory<Observatory, Integer>("numberOfGalamseyEvents"));
+
+      observeList.setItems(getObservatories());
+
+        biggestColorValue.setText(Integer.toString(Monitoring.getLargestGalamseyColourValueEverRecorded()));
+
+        if(!(Monitoring.getObservatoryWithLargestNumberOfEvents() == null)) {
+            maxGalamObservatory.setText(Monitoring.getObservatoryWithLargestNumberOfEvents().getName());
+        }else {
+            maxGalamObservatory.setText("Empty");
+        }
+
+
+        /*if(Monitoring.observatories.isEmpty()){
+            biggestColorValue.setText("0");
+            maxGalamObservatory.setText("Empty");
+        }else if (Monitoring.observatories.get(0).getEvents().isEmpty()){
+            biggestColorValue.setText(Integer.toString(Monitoring.getLargestGalamseyColourValueEverRecorded()));
+            maxGalamObservatory.setText(Monitoring.getObservatoryWithLargestAverage().getName());
+        }else {
+            biggestColorValue.setText(Integer.toString(Monitoring.getLargestGalamseyColourValueEverRecorded()));
+            //maxGalamObservatory.setText(Monitoring.getObservatoryWithLargestAverage().getName());
+        }*/
     }
 
     private ObservableList<Observatory> getObservatories() {
@@ -118,5 +186,25 @@ public class Controller implements Initializable {
         ob.addAll(Monitoring.observatories);
 
         return ob;
+    }
+
+    private void obsList(){
+        observa.add("Select an Observatory");
+        for (Observatory ob: Monitoring.observatories){
+            observa.add(ob.getName());
+        }
+    }
+
+    private int getIndexOfObservatory(String observatoryName){
+        int count = 0;
+        int index = 0;
+        for(Observatory observatory1: Monitoring.observatories){
+            if(observatory1.getName().equals(observatoryName)){
+                index = count;
+                break;
+            }
+            count = count +1;
+        }
+        return index;
     }
 }
---
+++ src/sample/Monitoring.java
@@ -0,0 +1,181 @@
+package sample;
+
+import java.util.*;
+
+/**
+ * This class holds information about all observatories
+ * @author Eugenia Mawuenya AKPO
+ * @author Michael Dankwah AGYEMAN-PREMPEH
+ * @author Issifu ALHASSAN
+ * @author Francis AWEEENAGUA
+ * @version 1.0.0
+ */
+public class Monitoring {
+    public static ArrayList<Observatory> observatories = new ArrayList<Observatory>();
+    /**
+     * The default Constructor
+     */
+    public Monitoring(){}
+    /**
+     * getObservatoryWithLargestAverage takes no parameters and returns the largest average recorded amongst the observatories
+     * @return observatory
+     */
+    private static ArrayList<Double> largestAverage=new ArrayList<Double>();
+    private static double highest=0;
+    private static Observatory observatory;
+    public static Observatory getObservatoryWithLargestAverage(){
+        for(Observatory i: observatories) {
+            largestAverage.add(i.averageGalamseyColourValueRecorded());
+            for(int j=0;j< largestAverage.size();j++) {if(largestAverage.get(j)>highest) {
+                highest = largestAverage.get(j);
+            }
+                if(i.averageGalamseyColourValueRecorded()==highest) { observatory = i;}
+            }
+        }
+        //System.out.println(observatory);
+        return observatory;
+    }
+    /**
+     * getLargestGalamseyColourValueEverRecorded is a method that returns the largest Galamsey colour value ever recorded
+     * @return largestGalamseyColourValue
+     */
+    private static   ArrayList<Integer> list= new ArrayList<Integer>();
+    private static   int largestGalamseyColourValue=0;
+    public static int getLargestGalamseyColourValueEverRecorded(){
+        for(Observatory i:observatories) {
+            list.add(i.largestGalamseyColourValueRecorded());
+            for (int j=0;j<list.size();j++) {
+                if(list.get(j) > largestGalamseyColourValue) {
+                    largestGalamseyColourValue=list.get(j);
+                }
+            }
+        }
+        //System.out.println(largestGalamseyColourValue);
+        return largestGalamseyColourValue;
+    }
+    /**
+     * This method returns a list of all Galamsey whose vegetation colour value is greater than some arbitrary constant
+     * @param number is the arbitrary constant
+     * @return List containing Galamsey events with coulour values less than "number"
+     */
+    public static ArrayList<Galamsey> ListOfGalamseysWithColourValueGreaterThan(double number) {
+        ArrayList<Galamsey> List=new ArrayList<Galamsey>();
+        for(Observatory i: observatories) {
+            List.addAll(i.galamseysWithColourValueGreaterThan(number));
+        }
+        //System.out.println(List);
+        return List;
+    }
+
+    /**
+     * This method returns a list of all Galamsey whose vegetation colour value is less than some arbitrary constant
+     * @param number is the arbitrary constant
+     * @return List containing Galamsey events with coulour values less than "number"
+     */
+
+    public static ArrayList<Galamsey> ListOfGalamseysWithColourValueLessThan(double number) {
+        ArrayList<Galamsey> List=new ArrayList<Galamsey>();
+        for(Observatory i: observatories) {
+            List.addAll(i.galamseysWithColourValueLesserThan(number));
+        }
+        //System.out.println(List);
+        return List;
+    }
+
+    /**
+     * This method returns a list of all Galamsey whose vegetation colour value is equal to some arbitrary constant
+     * @param number is the arbitrary constant
+     * @return List containing Galamsey events with coulour values equal to "number"
+     */
+
+    public static ArrayList<Galamsey> ListOfGalamseysWithColourValueEqualTo(double number) {
+        ArrayList<Galamsey> List=new ArrayList<Galamsey>();
+        for(Observatory i: observatories) {
+            List.addAll(i.galamseysWithColourValueEqualTo(number));
+        }
+        //System.out.println(List);
+        return List;
+    }
+    /**
+     * Checks the Observatory list for any observatory with a specified name.
+     * @param nameOfObservatory a string of the name of observatory you want to check.
+     * @return returns true if name is in observatory list, and false if not.
+     */
+    public static boolean checkObservatoryList(String nameOfObservatory){
+        boolean isItEqual = false;
+        for(Observatory anObservatory : observatories){
+            if(anObservatory.getName().equals(nameOfObservatory)){
+                isItEqual = true;
+                break;
+            }
+        }
+        return isItEqual;
+    }
+    public static Observatory getObservatoryWithLargestArea(){
+        ArrayList<Double> largestArea=new ArrayList<Double>();
+        double Area=0;
+        Observatory observatory1 = null;
+        for(Observatory i: observatories) {
+            largestArea.add(i.getAreaInSquareMeters());
+            for(int j=0;j< largestArea.size();j++) {if(largestArea.get(j)>Area) {
+                Area = largestArea.get(j);}
+                if(i.getAreaInSquareMeters()==Area) {observatory1 = i;}
+            }
+        }
+        System.out.println("Area is "+Area);
+        System.out.println("The observatory with the largest Area is "+observatory1);
+        return observatory1;
+    }
+
+    public static Observatory getObservatoryWithLargestNumberOfEvents() {
+        if (observatories.isEmpty()) {
+            return null;
+        }
+        Observatory obs = null;
+        int curLarge = 0;
+        for (Observatory i : observatories) {
+            if (i.getEvents().size() > curLarge) {
+                obs = i;
+            }
+        }
+
+        return obs;
+    }
+
+    /**
+     * Operations are executed in the main method
+     * @param args
+     */
+    public static void main(String[] args) {
+        Monitoring monitor =new Monitoring();
+        Galamsey g = new Galamsey ("brown",3);
+        Galamsey q = new Galamsey ("brown",3);
+        Galamsey r = new Galamsey ("yellow",2);
+        Galamsey y = new Galamsey ("green",1);
+        Galamsey u = new Galamsey ("green",1);
+        Galamsey p = new Galamsey ("brown",3);
+        Galamsey a = new Galamsey ("green",1);
+        Galamsey s = new Galamsey ("yellow",2);
+        Galamsey d = new Galamsey ("green",1);
+        Galamsey h = new Galamsey ("brown",3);
+        ArrayList<Galamsey> events =new ArrayList<Galamsey>();
+        ArrayList<Galamsey> event =new ArrayList<Galamsey>();
+        events.add(g);
+        events.add(q);
+        events.add(r);
+        event.add(y);
+        events.add(u);
+        event.add(p);
+        events.add(a);
+        event.add(s);
+        events.add(d);
+        event.add(h);
+        Observatory e = new Observatory("force","Ghana",2000, 50000,events);
+        Observatory t = new Observatory("force2","Togo",3000, 100000,event);
+        monitor.observatories.add(e);
+        monitor.observatories.add(t);
+        monitor.getObservatoryWithLargestAverage();
+        monitor.getLargestGalamseyColourValueEverRecorded();
+        monitor.ListOfGalamseysWithColourValueGreaterThan(0);
+    }
+}
